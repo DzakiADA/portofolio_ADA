b35dc760cb48a97465beba904597b10ea849e91d
Dzaki_ADA <dzakiadaaa@gmail.com>
2025-07-20 17:08:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,8 @@ form.addEventListener('submit', (e) => {
     successMsg.style.display = 'none';
   }, 3000);
 });
+
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
